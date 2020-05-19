a16eaff164b166bdf5d9a4e853c01e739e8c26f7
Amit Rungta <chehaktechnologies@gmail.com>
2020-05-19 15:19:49 +0530

    Added menu items in database

--- orders/admin.py
+++ orders/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from orders.models import Pizza
+from orders.models import Pizza , Sub , Dinner , SubExtra , Topping , Salad , Pasta
 
 # Register your models here.
 
 
 # Menu
 admin.site.register(Pizza)
+admin.site.register(Sub)
+admin.site.register(Dinner)
+admin.site.register(SubExtra)
+admin.site.register(Topping)
+admin.site.register(Pasta)
+admin.site.register(Salad)
--- orders/models.py
+++ orders/models.py
@@ -5,11 +5,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+
+
+
+
+#-----------------------------------------------------------------------
 # Base Item
 class BaseItem(models.Model):
     """Base class for items. This will contain the item basic properties."""
 
     name = models.CharField(max_length=64)
+    description = models.CharField(max_length=64,default="")
     SMALL = 'Small'
     LARGE = 'Large'
     SIZE_CHOICES = (
@@ -17,36 +23,141 @@ class BaseItem(models.Model):
         (LARGE, 'Large')
     )
     size = models.CharField(max_length=8, choices=SIZE_CHOICES, blank=True)
-    price = models.IntegerField(blank=True, default=0) # Blankable for Toppings
+    price = models.DecimalField(max_digits=6, decimal_places=2,blank=True, default=0) # Blankable for Toppings
 
     class Meta:
             abstract = True
 
 
     def __str__(self):
-        return f"{self.name} _ {self.price}"
+        if  ( {self.price} > 0 ) :
+            return f"{self.name} @ {self.price}"
+        else:
+            return f"{self.name}"
 
-    def is_valid_item(self):
-        return (self.price >= 0)
 
 
 
-class Pizza(BaseItem):
-    """ Defines a pizza. Pizza can have 2 types of bases."""
 
-    CRUST_CHOICES = (
-        ('REGULAR', 'Regular'),
-        ('SICILIAN', 'Sicilian')
-    )
+#-----------------------------------------------------------------------
+#
+class BaseItemWithPrice(BaseItem):
+    """Base class for items which requires a price compulsory"""
+
+    # here i am overriding the base class instance to make it that it cannot be left blank and it should have some price.
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+            abstract = True
+
+
+
+
+
+#-----------------------------------------------------------------------
+#
+class BaseItemWithPriceNSize(BaseItemWithPrice):
+    """Base class for items which requires a size compulsory"""
 
     # here i am overriding the base class instance to make it that it cannot be left blank and it should have some size.
     size = models.CharField(max_length=8, choices= BaseItem.SIZE_CHOICES, blank=False)
+
+    class Meta:
+            abstract = True
+
+    def __str__(self):
+        return f"{self.name} ({self.size}) @ {self.price}"
+
+
+
+
+#-----------------------------------------------------------------------
+#
+class Pizza(BaseItemWithPriceNSize):
+    """ Defines a pizza. Pizza can have 2 types of bases."""
+
+    REGULAR = 'Regular'
+    SICILIAN = 'Sicilian'
+    CRUST_CHOICES = (
+        (REGULAR, 'Regular'),
+        (SICILIAN, 'Sicilian')
+    )
 
     crust = models.CharField(max_length=8, choices=CRUST_CHOICES)
     max_toppings = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.crust} Pizza with {self.name} ({self.size}) _ {self.price}"
+        return f"{self.crust} Pizza with {self.name} ({self.size}) @ {self.price}"
+
+
+
+
+#-----------------------------------------------------------------------
+#
+class Dinner(BaseItemWithPriceNSize):
+    """ Defines a Dinner here."""
+
+    class Meta:
+        verbose_name = "Dinner Platter"
+        verbose_name_plural = "Dinner Platters"
+
+
+
+
+#-----------------------------------------------------------------------
+#
+class Sub(BaseItemWithPriceNSize):
+    """ Defines a sub here."""
+    pass
+
+
+
+
+#-----------------------------------------------------------------------
+#
+class SubExtra(BaseItemWithPriceNSize):
+    """Define extra-options."""
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+
+
+
+#-----------------------------------------------------------------------
+#
+class Topping(BaseItem):
+    """Define toppings."""
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+
+
+
+#-----------------------------------------------------------------------
+#
+class Pasta(BaseItemWithPrice):
+    """Define pastas."""
+    pass
+
+
+
+
+
+#-----------------------------------------------------------------------
+#
+class Salad(BaseItemWithPrice):
+    """Define salads."""
+    pass
+
+
+
+
+
+
 
 
 
